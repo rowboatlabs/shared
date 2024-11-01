e0d0a679f505ad06f08091c3d9724d11c57bbd6b
ramnique <30795890+ramnique@users.noreply.github.com>
2024-11-01 15:37:25 +0530

    foo

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,1 @@
-import * as apiV1Types from './types/api_v1';
-export declare const types: {
-    apiV1: typeof apiV1Types;
-};
+export * from './types/api_v1';
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,1 @@
-import * as apiV1Types from './types/api_v1';
-export const types = {
-    apiV1: apiV1Types
-};
+export * from './types/api_v1';
